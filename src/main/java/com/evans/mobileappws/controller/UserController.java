50336de0b80eefad1b76295c01d1c1ee2ec9a2e5
Evans Shango <lutatwaevans@gmail.com>
2019-12-24 21:51:53 +0300

    check for duplicate entries using the user provided email

--- src/main/java/com/evans/mobileappws/controller/UserController.java
+++ src/main/java/com/evans/mobileappws/controller/UserController.java
@@ -1,30 +1,45 @@
 package com.evans.mobileappws.controller;
 
+import com.evans.mobileappws.dto.UserDto;
+import com.evans.mobileappws.model.request.UserDetails;
+import com.evans.mobileappws.model.response.UserRest;
+import com.evans.mobileappws.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("api")
 @AllArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @GetMapping
-    public String getUser(){
+    public String getUser() {
         return "get user was called";
     }
 
-    @PostMapping
-    public String createUser(){
-        return "create user was called";
+    @PostMapping("/create-user")
+    public UserRest createUser(@RequestBody UserDetails userDetails) {
+        UserRest userRest = new UserRest();
+
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+
+        UserDto createdUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, userRest);
+
+        return userRest;
     }
 
     @PutMapping
-    public String updateUser(){
+    public String updateUser() {
         return "update user was called";
     }
 
     @DeleteMapping
-    public String deleteUser(){
+    public String deleteUser() {
         return "delete user was called";
     }
 }
