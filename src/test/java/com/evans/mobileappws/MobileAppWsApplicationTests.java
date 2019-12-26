7441161ced3b5b5fe9a4c937bb991667dd850603
Evans Shango <lutatwaevans@gmail.com>
2019-12-26 20:10:46 +0300

    handle test compile errors in the main test class

--- src/test/java/com/evans/mobileappws/MobileAppWsApplicationTests.java
+++ src/test/java/com/evans/mobileappws/MobileAppWsApplicationTests.java
@@ -1,9 +1,13 @@
 package com.evans.mobileappws;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {MobileAppWsApplication.class})
 class MobileAppWsApplicationTests {
 
     @Test
